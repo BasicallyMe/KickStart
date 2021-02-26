bd56a276737800c057ed98afa7332a71d53129ed
Abajit Dey <78209391+BasicallyMe@users.noreply.github.com>
2021-02-26 22:35:28 +0530

    Input changes
    Added new input statements for number of houses and the budget available and also for the price of each house.

--- practice.java
+++ practice.java
@@ -1,5 +1,5 @@
 package com.java.Practice;
-
+import java.util.Scanner;
 class House {
     int count=0;
     void sortArray(int[] array, int size) {
@@ -30,10 +30,17 @@ class House {
 }
 class Calc {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         House h1 = new House();
-        int size = 4;
-        int budget = 100;
-        int[] array = {20,90,40,90};
+        System.out.print("Enter the number of houses available");
+        int size = input.nextInt();
+        System.out.print("\nEnter the budget available");
+        int budget = input.nextInt();
+        int[] array = new int[size];
+        System.out.print("\nEnter the elements of the array");
+        for(int i=0; i<size; i++) {
+            array = input.nextInt();
+        }
         h1.sortArray(array, size);
         int number = h1.setCount(array, size, budget);
         System.out.print("\nThe number of houses that can be bought within the budget is "+number);
